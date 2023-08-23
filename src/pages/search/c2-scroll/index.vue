f1cc6898cdc8a74eefd5943085b5bea3defea13a
mid <365561171@qq.com>
2023-08-23 22:22:31 +0800

    backup

--- src/pages/search/c2-scroll/index.vue
+++ src/pages/search/c2-scroll/index.vue
@@ -1,9 +1,7 @@
 <template>
-  <u-scroll-list>
-    <view v-for="(item, index) in c2List" :key="index">
-      <image :src="item.thumb"></image>
-    </view>
-  </u-scroll-list>
+  <scroll-view croll-y="true" scroll-y="true" class="scroll-Y">
+    <div v-for="item in c2List" :key="item" class="item">{{ item.name }}</div>
+  </scroll-view>
 </template>
 
 <script setup lang="ts">
@@ -11,4 +9,23 @@ import { defineProps } from 'vue'
 defineProps(['c2List'])
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$theSizeOfFont: 26rpx;
+.scroll-Y {
+  height: 100%;
+  width: 100%;
+  background-color: $mini-gray;
+  .item {
+    padding: 16rpx;
+    font-size: $theSizeOfFont;
+    color: gray;
+    text-align: center;
+    & .active {
+        background-color: white;
+        color: black;
+        font-weight: 600;
+        
+    }
+  }
+}
+</style>
--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <u-search
-    height="70rpx"
-    :showAction="false"
-    :clearabled="true"
-    placeholder="请输入产品名称/型号"
-    v-model="queryWrapper.keyword"
-  ></u-search>
+  <view class="search-container">
+    <u-search
+      height="70rpx"
+      :showAction="false"
+      :clearabled="true"
+      placeholder="请输入产品名称/型号"
+      v-model="queryWrapper.keyword"
+    ></u-search>
+  </view>
+
+  <C2Scroll class="mid-c2-scroll" :c2List="c2List" />
 </template>
 
 <script setup lang="ts">
-import { getC2ListByC1Id } from '../../api/search';
+import C2Scroll from './c2-scroll'
+import { onLoad } from '@dcloudio/uni-app'
+import { getC2ListByC1Id } from '../../api/search'
 import { ref, reactive } from 'vue'
 let queryWrapper = reactive({
   c1Id: undefined,
@@ -19,6 +25,27 @@ let queryWrapper = reactive({
   pageNumber: 1,
   keyword: ''
 })
+let c2List = ref([])
+let c3List = ref([])
+onLoad((options) => {
+  queryWrapper.c1Id = '墙板'
+  initC2List()
+})
+const initC2List = () => {
+  getC2ListByC1Id(queryWrapper.c1Id).then((r) => {
+    c2List.value = r
+  })
+}
+const initC3List = () => {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.search-container {
+  padding: 10rpx;
+  box-shadow: 0 0 10rpx #333;
+  margin-bottom: 15rpx;
+}
+.mid-c2-scroll {
+  width: 180rpx;
+}
+</style>
