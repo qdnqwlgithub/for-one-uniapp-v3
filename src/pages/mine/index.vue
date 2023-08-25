c2f257bda039eda0ca1c7a2aae9b7b6ba6879dba
mid <365561171@qq.com>
2023-08-25 18:25:52 +0800

    backup

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,28 +1,26 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-   <u-cell-group>
-	<u-cell  title="产品收藏" arrow-direction="down">
-		<template #icon>
-			<u-icon size="32" name="star"></u-icon>
-		</template>
-		<!-- <u-badge count="99" :absolute="false" slot="right-icon"></u-badge> -->
-		<template #right-icon>
-            <u-icon size="32" name="arrow-up"></u-icon>
-		</template>
-	</u-cell>
-    <u-cell  title="案例收藏" arrow-direction="down">
-		<template #icon>
-			<u-icon size="32" name="star-fill"></u-icon>
-		</template>
-		<!-- <u-badge count="99" :absolute="false" slot="right-icon"></u-badge> -->
-		<template #right-icon>
-            <u-icon size="32" name="arrow-up"></u-icon>
-		</template>
-	</u-cell>
-</u-cell-group>
+  <u-cell-group>
+    <u-cell title="产品收藏" arrow-direction="down">
+      <template #icon>
+        <u-icon size="32" name="star"></u-icon>
+      </template>
+      <!-- <u-badge count="99" :absolute="false" slot="right-icon"></u-badge> -->
+      <template #right-icon>
+        <u-icon size="32" name="arrow-up"></u-icon>
+      </template>
+    </u-cell>
+    <u-cell title="案例收藏" arrow-direction="down">
+      <template #icon>
+        <u-icon size="32" name="star-fill"></u-icon>
+      </template>
+      <!-- <u-badge count="99" :absolute="false" slot="right-icon"></u-badge> -->
+      <template #right-icon>
+        <u-icon size="32" name="arrow-up"></u-icon>
+      </template>
+    </u-cell>
+  </u-cell-group>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
